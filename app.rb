bd7331a22dd815d32ec17b5511744decaed8dd44
Louis Aurick <louisaurick@ntymail.com>
2018-01-23 15:32:58 +0100

    exercice1

--- app.rb
+++ app.rb
@@ -1,1 +1,18 @@
-10.times {puts "hello world"}
+for num in 1..10
+	puts "hello human"
+end
+
+i = 1
+while i <= 10
+	puts "Hello stranger"
+	i += 1
+end
+
+t = 0
+until t == 10
+	puts "Bonjour monde"
+	t += 1
+end
+
+
+(0..10).each {puts "coucou"}
